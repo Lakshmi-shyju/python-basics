6c1cc24d176e2ac5abb954925171a3d89aac3389
Lakshmi-shyju <132272516+Lakshmi-shyju@users.noreply.github.com>
2023-05-01 18:31:56 +0530

    Add files via upload
    initial commit

---
+++ variables.py
@@ -0,0 +1,31 @@
+# x = 5
+# y = "John"
+
+# print(x)
+# print(y)
+
+# x = str(3)
+# print(x)
+# print(type(x))
+
+# y = int(3)    # y will be 3
+# z = float(3)  # z will be 3.0
+
+# print(type(y))
+# print(type(z))
+
+# carname = "Volvo"
+# print(carname)
+
+# x = "Python is awesome"
+# print(x)
+
+# x = "Python"
+# y = "is"
+# z = "awesome"
+# print(x, y, z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
